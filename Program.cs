007e633ab8db319bbdfc1a493edf5e892f9d1626
Grant Linehan <grantlinehan@gmail.com>
2023-03-13 13:50:06 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string csvPath = "C:\\Users\\grant\\source\\repos\\Ticketing System\\Ticketing System\\tickets.csv";
+
+List<string> newEntry = new List<string>(); 
+
+newEntry.Add(System.IO.File.ReadAllLines(csvPath).Length + 1 + ",");
+
+Console.Write("Please summarize your issue here\n > "); newEntry[0] += Console.ReadLine() + ",";
+
+Console.Write("\nPlease enter the status of your issue (Open, Closed)\n > "); newEntry[0] += Console.ReadLine() + ",";
+
+Console.Write("\nPlease enter the priority of your issue (High, Medium, Low)\n > "); newEntry[0] += Console.ReadLine() + ",";
+
+Console.Write("\nPlease enter your name\n > "); newEntry[0] += Console.ReadLine() + ",";
+
+Console.Write("\nPlease enter who was assigned to your issue\n > "); newEntry[0] += Console.ReadLine() + ",";
+
+Console.Write("\nPlease enter who is watching your issue (please seperate people with the '|' character)\n > "); newEntry[0] += Console.ReadLine();
+
+File.AppendAllLines(csvPath, newEntry);
